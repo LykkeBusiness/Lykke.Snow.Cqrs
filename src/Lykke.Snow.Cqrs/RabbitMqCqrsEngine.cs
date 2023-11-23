4bc9babd973be31b2ee249c23905b423abfab98a
atarutin <andrey.tarutin@lykke-business.ch>
2023-11-23 16:18:22 +0300

    Revert "fix(LT-4283): migrate to ms logging due to dependencies update"
    This reverts commit 13038693bbd83f5f9f9567e0871d9edb6c694fc1.

--- src/Lykke.Snow.Cqrs/RabbitMqCqrsEngine.cs
+++ src/Lykke.Snow.Cqrs/RabbitMqCqrsEngine.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Collections.Generic;
+using Common.Log;
 using JetBrains.Annotations;
+using Lykke.Common.Log;
 using Lykke.Cqrs;
 using Lykke.Cqrs.Configuration;
 using Lykke.Messaging;
 using Lykke.Messaging.Configuration;
 using Lykke.Messaging.Contract;
 using Lykke.Messaging.RabbitMq;
-using Microsoft.Extensions.Logging;
 
 namespace Lykke.Snow.Cqrs
 {
@@ -17,48 +19,127 @@ namespace Lykke.Snow.Cqrs
     [PublicAPI]
     public class RabbitMqCqrsEngine : CqrsEngine
     {
-        public RabbitMqCqrsEngine(ILoggerFactory loggerFactory,
+        public RabbitMqCqrsEngine(ILog log,
             string endpoint,
             string username,
             string password,
-            params IRegistration[] registrations) : base(loggerFactory,
-            CreateMessagingEngine(loggerFactory, endpoint, username, password), registrations)
+            params IRegistration[] registrations) : base(log,
+            CreateMessagingEngine(log, endpoint, username, password), registrations)
         {
         }
 
-        public RabbitMqCqrsEngine(ILoggerFactory loggerFactory,
+        public RabbitMqCqrsEngine(ILog log,
             IEndpointProvider endpointProvider,
             string endpoint,
             string username,
             string password,
-            params IRegistration[] registrations) : base(loggerFactory,
-            CreateMessagingEngine(loggerFactory, endpoint, username, password), endpointProvider,
-            registrations)
+            params IRegistration[] registrations) : base(log,
+            CreateMessagingEngine(log, endpoint, username, password), endpointProvider, registrations)
         {
         }
 
-        public RabbitMqCqrsEngine(ILoggerFactory loggerFactory,
+        public RabbitMqCqrsEngine(ILog log,
             IDependencyResolver dependencyResolver,
             IEndpointProvider endpointProvider,
             string endpoint,
             string username,
             string password,
-            params IRegistration[] registrations) : base(loggerFactory, dependencyResolver,
-            CreateMessagingEngine(loggerFactory, endpoint, username, password), endpointProvider,
-            registrations)
+            params IRegistration[] registrations) : base(log, dependencyResolver,
+            CreateMessagingEngine(log, endpoint, username, password), endpointProvider, registrations)
         {
         }
 
-        public RabbitMqCqrsEngine(ILoggerFactory loggerFactory,
+        public RabbitMqCqrsEngine(ILog log,
             IDependencyResolver dependencyResolver,
             IEndpointProvider endpointProvider,
             string endpoint,
             string username,
             string password,
             bool createMissingEndpoints,
-            params IRegistration[] registrations) : base(loggerFactory, dependencyResolver,
-            CreateMessagingEngine(loggerFactory, endpoint, username, password), endpointProvider,
+            params IRegistration[] registrations) : base(log, dependencyResolver,
+            CreateMessagingEngine(log, endpoint, username, password), endpointProvider,
             createMissingEndpoints, registrations)
+        {
+        }
+
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            string endpoint,
+            string username,
+            string password,
+            params IRegistration[] registrations) : base(logFactory,
+            CreateMessagingEngine(logFactory, endpoint, username, password), registrations)
+        {
+        }
+
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            IEndpointProvider endpointProvider,
+            string endpoint,
+            string username,
+            string password,
+            params IRegistration[] registrations) : base(logFactory,
+            CreateMessagingEngine(logFactory, endpoint, username, password), endpointProvider,
+            registrations)
+        {
+        }
+
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            IDependencyResolver dependencyResolver,
+            IEndpointProvider endpointProvider,
+            string endpoint,
+            string username,
+            string password,
+            params IRegistration[] registrations) : base(logFactory, dependencyResolver,
+            CreateMessagingEngine(logFactory, endpoint, username, password), endpointProvider,
+            registrations)
+        {
+        }
+
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            IDependencyResolver dependencyResolver,
+            IEndpointProvider endpointProvider,
+            string endpoint,
+            string username,
+            string password,
+            bool createMissingEndpoints,
+            params IRegistration[] registrations) : base(logFactory, dependencyResolver,
+            CreateMessagingEngine(logFactory, endpoint, username, password), endpointProvider,
+            createMissingEndpoints, registrations)
+        {
+        }
+
+        [Obsolete("Please, take care of messaging engine disposal")]
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            IMessagingEngine messagingEngine,
+            params IRegistration[] registrations) : base(logFactory, messagingEngine, registrations)
+        {
+        }
+
+        [Obsolete("Please, take care of messaging engine disposal")]
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            IMessagingEngine messagingEngine,
+            IEndpointProvider endpointProvider,
+            params IRegistration[] registrations) : base(logFactory, messagingEngine, endpointProvider, registrations)
+        {
+        }
+
+        [Obsolete("Please, take care of messaging engine disposal")]
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            IDependencyResolver dependencyResolver,
+            IMessagingEngine messagingEngine,
+            IEndpointProvider endpointProvider,
+            params IRegistration[] registrations) : base(logFactory, dependencyResolver, messagingEngine,
+            endpointProvider, registrations)
+        {
+        }
+
+        [Obsolete("Please, take care of messaging engine disposal")]
+        public RabbitMqCqrsEngine(ILogFactory logFactory,
+            IDependencyResolver dependencyResolver,
+            IMessagingEngine messagingEngine,
+            IEndpointProvider endpointProvider,
+            bool createMissingEndpoints,
+            params IRegistration[] registrations) : base(logFactory, dependencyResolver, messagingEngine,
+            endpointProvider, createMissingEndpoints, registrations)
         {
         }
 
@@ -71,14 +152,24 @@ namespace Lykke.Snow.Cqrs
             }
         }
 
-        private static IMessagingEngine CreateMessagingEngine(ILoggerFactory loggerFactory,
+        private static IMessagingEngine CreateMessagingEngine(ILogFactory logFactory,
             string rabbitMqEndpoint,
             string rabbitMqUserName,
             string rabbitMqPassword)
         {
             var transportResolver = CreateTransport(rabbitMqEndpoint, rabbitMqUserName, rabbitMqPassword);
 
-            return new MessagingEngine(loggerFactory, transportResolver, new RabbitMqTransportFactory(loggerFactory));
+            return new MessagingEngine(logFactory, transportResolver, new RabbitMqTransportFactory());
+        }
+
+        private static IMessagingEngine CreateMessagingEngine(ILog log,
+            string rabbitMqEndpoint,
+            string rabbitMqUserName,
+            string rabbitMqPassword)
+        {
+            var transportResolver = CreateTransport(rabbitMqEndpoint, rabbitMqUserName, rabbitMqPassword);
+
+            return new MessagingEngine(log, transportResolver, new RabbitMqTransportFactory());
         }
 
         private static ITransportResolver CreateTransport(string rabbitMqEndpoint,
